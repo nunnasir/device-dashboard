34a71232dba4e5fec9a6bd5e53de37470863b106
nunnasir <nunnasir@gmail.com>
2024-05-21 14:13:20 +0600

    Update dashboar

--- src/ProjectBoard.jsx
+++ src/ProjectBoard.jsx
@@ -30,7 +30,7 @@ export default function ProjectBoard() {
 
       {/* Input Fields */}
       <div className="flex flex-wrap mb-4">
-        <div className="w-full md:w-1/3 px-2 mb-4 md:mb-0">
+        {/* <div className="w-full md:w-1/3 px-2 mb-4 md:mb-0">
           <label htmlFor="fieldId" className="block text-gray-700">
             Field Id
           </label>
@@ -46,7 +46,7 @@ export default function ProjectBoard() {
               </option>
             ))}
           </select>
-        </div>
+        </div> */}
         <div className="w-full md:w-1/3 px-2 mb-4 md:mb-0">
           <label htmlFor="dropdown" className="block text-gray-700">
             Crop Name
