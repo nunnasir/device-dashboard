4c5ae81ccf9216b8625b38c8f0350cd4263a37e0
nunnasir <nunnasir@gmail.com>
2024-05-21 10:09:36 +0600

    Update Standard Value

--- src/provider/DeviceDataProvider.jsx
+++ src/provider/DeviceDataProvider.jsx
@@ -5,11 +5,11 @@ import { DeviceDataContext } from "../context";
 import { useDeviceData } from "../hooks";
 
 const DeviceDataProvider = ({ children }) => {
-  const { deviceData, deviceStandardeData, error, loading } = useDeviceData();
+  const { deviceData, deviceStandardData, error, loading } = useDeviceData();
 
   return (
     <DeviceDataContext.Provider
-      value={{ deviceData, deviceStandardeData, error, loading }}
+      value={{ deviceData, deviceStandardData, error, loading }}
     >
       {children}
     </DeviceDataContext.Provider>
