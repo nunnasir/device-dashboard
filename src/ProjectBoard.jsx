3f4b680d9a1d0875714c10c3e2abc59f7b255f89
nunnasir <nunnasir@gmail.com>
2024-05-21 13:55:55 +0600

    Update dashboar

--- src/ProjectBoard.jsx
+++ src/ProjectBoard.jsx
@@ -15,8 +15,6 @@ export default function ProjectBoard() {
   const { selectedField, setSelectedField } = useContext(FieldIdContext);
 
   const handleChange = (event) => {
-    console.log(event.target.value);
-
     setSelectedField(event.target.value);
   };
 
