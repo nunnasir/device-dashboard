1ea56e51a566942fa117602c59d7a95ccbd3dc97
nunnasir <nunnasir@gmail.com>
2024-05-21 13:50:57 +0600

    Update dashboar

--- src/ProjectBoard.jsx
+++ src/ProjectBoard.jsx
@@ -54,7 +54,7 @@ export default function ProjectBoard() {
             className="w-full border rounded-md px-3 py-2"
           >
             {fieldInfo.map((field) => (
-              <option key={field.id} value={field.name}>
+              <option key={field.id} value={field.id}>
                 {field.name}
               </option>
             ))}
